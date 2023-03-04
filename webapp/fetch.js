fff40a9b707c3c2f4935b882906a8b18233cb4cc
Maxime <121681048+xamidev@users.noreply.github.com>
2023-03-04 16:04:40 +0100

    fetching info for 3 basic values

--- webapp/fetch.js
+++ webapp/fetch.js
@@ -1,1 +1,15 @@
-/* fetch data from circuit TBD */
+setInterval(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'bme.txt', true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var measures = xhr.responseText.split('\n\n');
+        var lastMeasures = measures[measures.length - 1];
+        lastMeasures = lastMeasures.split('\n');
+        document.getElementById('temperature').innerHTML = lastMeasures[lastMeasures.length - 5]
+        document.getElementById('pressure').innerHTML = lastMeasures[lastMeasures.length - 4]
+        document.getElementById('humidity').innerHTML = lastMeasures[lastMeasures.length - 3]
+      }
+    };
+    xhr.send();
+  }, 2000);
