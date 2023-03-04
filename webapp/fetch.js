40277b20d67f86d281274025f60b93662cdae899
Maxime <121681048+xamidev@users.noreply.github.com>
2023-03-04 17:34:11 +0100

    added chart.js basic mock valued graph

--- webapp/fetch.js
+++ webapp/fetch.js
@@ -6,10 +6,38 @@ setInterval(function() {
         var measures = xhr.responseText.split('\n\n');
         var lastMeasures = measures[measures.length - 1];
         lastMeasures = lastMeasures.split('\n');
-        document.getElementById('temperature').innerHTML = lastMeasures[lastMeasures.length - 5]
-        document.getElementById('pressure').innerHTML = lastMeasures[lastMeasures.length - 4]
-        document.getElementById('humidity').innerHTML = lastMeasures[lastMeasures.length - 3]
+        document.getElementById('temperature').innerHTML = lastMeasures[0]
+        document.getElementById('pressure').innerHTML = lastMeasures[1]
+        document.getElementById('humidity').innerHTML = lastMeasures[2]
+        document.getElementById('airvpd').innerHTML = lastMeasures[3]
+        document.getElementById('leafvpd').innerHTML = lastMeasures[4]
+        document.getElementById('dewpoint').innerHTML = lastMeasures[5]
+
       }
     };
     xhr.send();
   }, 2000);
+
+const lineCanvas = document.getElementById("lineCanvas");
+
+const lineChart = new Chart(lineCanvas, {
+    type: "line",
+    data: {
+        labels: ["T-7", "T-6", "T-5", "T-4", "T-3", "T-2", "T-1", "T"],
+        datasets: [{
+            label: "Temperature",
+            data: [240,120,140,130,122,182,79,81],
+            borderColor: "green",
+            backgroundColor: "lightgreen"
+        },
+        {
+            label: "Humidity",
+            data: [28,47,141,72,121,109,175,97],
+            borderColor: "crimson",
+            backgroundColor: "red"
+        }]
+    },
+    options: {
+
+    }
+});
