05177c267943296fd7556795b0eb47bef799f712
Maxime <121681048+xamidev@users.noreply.github.com>
2023-03-12 11:49:44 +0100

    added support for MQ2 sensor and OLED

--- fetching/bme.py
+++ fetching/bme.py
@@ -1,49 +1,56 @@
-# Hardware I/O imports
-from machine import Pin, I2C  
-import bme280
-
-# Other imports
+from machine import I2C, Pin
+from ssd1306 import SSD1306_I2C
 from time import sleep
-from math import e, log
+import bme280
+from bh1750 import BH1750
+from mq2 import MQ2
 
-i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)   
-internalLED = machine.Pin(25,machine.Pin.OUT)
-externalLED = machine.Pin(13,machine.Pin.OUT)
-internalLED.value(1)
+bootLed = machine.Pin(4, machine.Pin.OUT)
+bootLed.value(1)
 
-# Blink function for LEDs in circuit
-def blink(led):
-    led.value(1)
-    sleep(0.1)
-    led.value(0)
+i2c = I2C(0,sda=Pin(16), scl=Pin(17), freq=400000)
+i2c_lux = I2C(1,sda=Pin(14), scl=Pin(15), freq=400000)
+light = BH1750(i2c_lux)
+internalLed = machine.Pin(25, machine.Pin.OUT)
+measureLed = machine.Pin(13, machine.Pin.OUT)
 
-# Main thread loop
+pin=26
+sensor = MQ2(pinData = pin, baseVoltage = 3.3)
+oled = SSD1306_I2C(128,64,i2c)
+
+print("Calibrating")
+sensor.calibrate()
+print("Calibration completed")
+print("Base resistance:{0}".format(sensor._ro))
+
+internalLed.value(1)
+
 while True:
     try:
-        # Fetch information from BME280
         bme = bme280.BME280(i2c=i2c)
-        blink(externalLED)
         temp = bme.values[0][:-1]
         pressure = bme.values[1]
         humidity = bme.values[2]
-        rh = bme.values[2][:-1]
-        leaftemp = float(temp)-2.0
-        #print(f"Temp: {temp}°C | Pressure: {pressure} | Humidity: {bme.values[2]}")
+        lux = round(light.luminance(BH1750.ONCE_HIRES_1))
+        gas = sensor.readSmoke()
         
-        airSVP = 610.78*e**(float(temp)/(float(temp)+237.3)*17.2694)
-        airVPD = airSVP*(1-float(rh)/100)
         
-        leafSVP = 610.78*e**(leaftemp/(leaftemp+237.3)*17.2694)
-        leafVPD = leafSVP - (airSVP*float(rh)/100)
+        oled.fill(0)
+        oled.text(f"T: {temp}C",0,0)
+        oled.text(f"H: {humidity}", 0,15)
+        oled.text(f"P: {pressure}", 0,30)
+        oled.text(f"G: {round(gas,1)} ppm", 0,45)
+        oled.text(f"{lux} lux",0,55)
+        oled.show()
+        measureLed.value(1)
+        sleep(0.1)
+        measureLed.value(0)
+        bootLed.value(0)
         
-        atrh = log(float(rh)/100) + (17.625*float(temp))/(243.04+float(temp))
-        dewPoint = (243.04 * atrh / (17.625 - atrh))
-        #print(f"Air VPD: {round(airVPD/1000,3)}kPa | Leaf VPD: {round(leafVPD/1000,3)}kPa | Dew point: {round(dewPoint,2)}°C\n")
-        print(f"{temp}°C\n{pressure}\n{bme.values[2]}\n{round(airVPD/1000,3)}kPa\n{round(leafVPD/1000,3)}kPa\n{round(dewPoint,2)}°C\n")
-        sleep(1)
-    # On thread exit, shut LEDs off
-    except (KeyboardInterrupt, SystemExit):
-        print("KeyboardInterrupt caught - Ending thread")
-        internalLED.value(0)
-        externalLED.value(0)
+    except KeyboardInterrupt:
+        internalLed.value(0)
+        bootLed.value(0)
+        measureLed.value(0)
+        print("Abort")
+        oled.poweroff()
         raise
